c574b9755f22822e5f5a58365a9e645f0de354ab
Lucas <llucasanttiago93@gmail.com>
2026-04-24 16:04:59 -0300

    ajuste porta render

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.get("/cep", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando");
+});
   console.log("Servidor rodando em http://localhost:3000");
 });
