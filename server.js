60d799d5dc43608d9bdfcb7eea11688f6bb33c95
Lucas <llucasanttiago93@gmail.com>
2026-04-24 16:28:49 -0300

    Corrigindo erro de sintaxe

--- server.js
+++ server.js
@@ -2,15 +2,22 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-app.use(express.json());
 
 app.get("/cep", async (req, res) => {
   const cep = req.query.cep;
 
-  try {
-    const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+  if (!cep) {
+    return res.json({ erro: "CEP não enviado" });
+  }
 
-    if (cep.startsWith("0484") || cep.startsWith("0485") || cep.startsWith("0486")) {
+  try {
+    await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+
+    if (
+      cep.startsWith("0484") ||
+      cep.startsWith("0485") ||
+      cep.startsWith("0486")
+    ) {
       return res.json({ resultado: "frete_gratis" });
     } else {
       return res.json({ resultado: "frete_normal" });
@@ -25,6 +32,4 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Servidor rodando");
-});
-  console.log("Servidor rodando em http://localhost:3000");
 });
